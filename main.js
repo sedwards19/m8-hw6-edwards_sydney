9475a0a2ecf5e5d8de060bb35fc2d17771a7550c
Sydney Edwards <sydneyedwards@Sydneys-Air.lan>
2018-10-14 14:33:56 -0400

    Set up variables for yourFortune and fortuneTextPlaceholder

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
+var button = document.getElementsByTagName('button')[0];
+
+var yourFortune = document.querySelector('#fortune-wrapper p span');
+
+var fortuneTextPlaceholder = document.createTextNode('');
+yourFortune.appendChild(fortuneTextPlaceholder);
+
+//Create Random List
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
+//Make the function
 var shakeBall = function() {
 	magicFortunes.sort(function(a, b){return 0.5 - Math.random()});
 }
 
-var button = document.getElementsByTagName('button')[0];
-
+//Event Handler Property
 button.onclick = shakeBall;
